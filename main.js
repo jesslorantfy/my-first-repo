364da2ea170eb554ac129d37a721a4e8b93e424a
Jess Lorantfy <jessicalorantfy@gmail.com>
2017-11-18 20:24:45 -0500

    Finished first version

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello World!!")
+// Dependencies
+var prompt = require('prompt');
+
+// Start the prompt
+prompt.start();
+
+// Get two numbers from user
+prompt.get(['num1', 'num2'], function (err, result) {
+    getGreatestCommonFactor(result.num1, result.num2)
+});
+
+function getGreatestCommonFactor(num1, num2) {
+    console.log("Highest common factor!")
+    var gcf = 1;
+    for(var i = 1; i < num1 && i < num2; i++) {
+        if (num2 % i == 0 && num1 % i == 0) {
+            gcf = i;
+        }
+    }
+    
+    console.log(`The greatest common factor between ${num1} and ${num2} is ${gcf}`);    
+}
+
+
